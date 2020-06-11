f1011dcf62cce06ac20c1a8808ada32115f19df1
Mtuhiko Gusikenn <chanmitsu35@gmail.com>
2020-06-11 16:42:30 +0900

    写真が変わるようにしました。

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
     <div>
     <?php if ( get_header_image() ) : ?>
-	    <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
+      <img src="<?php header_image(); ?>"  alt="" class="top-image">
+      <?php else: ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/dog.jpg" alt="犬" class="top-image">
     <?php endif; ?>
-
-      <img src="<?php echo get_template_directory_uri(); ?>/img/dog.jpg" alt="犬" class="top-image">
     </div>
 
     <section>
