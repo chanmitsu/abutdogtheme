71a6027550fae2030881a9361aec8d427c52f6ab
MiyoshiFumiko <rhapsody.in.blue926@gmail.com>
2020-06-11 15:39:41 +0900

    ヘッダー画像とブラウザのタイトルを変更可能に

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+//このテーマでサポートする機能の設定
+//投稿のアイキャッチ画像サポート
+add_theme_support('post-thumbnails');
+
+//カスタムヘッダ―の読み込み
+add_theme_support( 'custom-header' );
+
+add_theme_support( 'title-tag' );
+
